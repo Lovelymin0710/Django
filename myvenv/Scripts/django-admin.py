2512fa3bda11351750596c57f86c86e59417ec99
경민너구리 <48938663+Lovelymin0710@users.noreply.github.com>
2019-10-28 22:50:22 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\olramde\desktop\djangoapiproject-master\djangoapiproject-master\proj\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
